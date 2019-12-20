715dc7856cd8952a30c5cdc78fc43575cae46d69
quanboy <46613544+quanboy@users.noreply.github.com>
2019-12-20 00:09:27 -0500

    Add files via upload

---
+++ Trump.py
@@ -0,0 +1,10 @@
+import requests
+import json
+from newsapi import NewsApiClient
+
+newsapi = NewsApiClient(api_key='24c82007b3984a03acfafa571aed921b')
+
+top_headlines = newsapi.get_top_headlines(q='trump',
+                                      language='en',
+                                         )
+print(top_headlines)
